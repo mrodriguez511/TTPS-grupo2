de26d1e94dcdb480221c34f75d79a3cbca6523a7
Mariana Rodriguez <mar.rodriguez-@hotmail.com>
2021-10-27 22:45:56 -0300

    tabla estudio y demas

--- app/models/punto_encuentro.py
+++ app/models/punto_encuentro.py
@@ -60,20 +60,6 @@ class DiagnosticoPresuntivo(db.Model):
         self.nombre = nombre
 
 
-class Extraccion(db.Model):
-
-    __tablename__ = "extracciones"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    realizada = Column(Boolean)
-    abonada = Column(Boolean)
-    precio = Column(Integer)
-    # permisos = relationship("Permiso", secondary=roles_permisos, back_populates="roles")
-
-    def __init__(self):
-        self.realizada = False
-        self.abonada = False
-
-
 class MedicoDerivante(db.Model):
 
     __tablename__ = "medicosDerivantes"
