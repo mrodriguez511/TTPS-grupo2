086026fc01ec9ce1ab7bd11101cd6f31e16aba68
mrodriguez511 <88698043+mrodriguez511@users.noreply.github.com>
2021-11-14 04:00:20 -0300

    Update loadData.py

--- app/loadData.py
+++ app/loadData.py
@@ -161,9 +161,9 @@ def cargarTiposDeEstudio():
 
 
 
-def cargarDiagonosticos():
+"""def cargarDiagonosticos():
     with open("archivos/Patologias.csv") as data_set:
         reader = csv.reader(data_set)
         encabezado = next(reader)
         for fila in reader:
-            db.session.add(DiagnosticoPresuntivo(nombre=fila))
+            db.session.add(DiagnosticoPresuntivo(nombre=fila))"""
